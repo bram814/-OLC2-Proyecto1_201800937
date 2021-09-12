6c3edcd094e9d408271c13f53713a0068df56352
Abraham Solórzano <abraham81423@gmail.com>
2021-09-12 15:21:48 -0600

    [ADD] Sentencia de Transferencia Break.

---
+++ Backend/src/Interprete/Instrucciones/Break.py
@@ -0,0 +1,12 @@
+from Interprete.Abstract.Instruccion import Instruccion
+
+class Break(Instruccion):
+    def __init__(self, fila, columna):
+        self.fila = fila
+        self.columna = columna
+
+    def interpretar(self, tree, table):
+        return self
+        
+    def AST(self):
+        pass
