841f829470bcdcc9b83330160f5025712603c8cd
Abraham Solórzano <abraham81423@gmail.com>
2021-09-12 15:33:54 -0600

    [ADD] Condicional If.

---
+++ Backend/src/Interprete/Instrucciones/If.py
@@ -0,0 +1,58 @@
+from Interprete.Abstract.Instruccion import Instruccion
+from Interprete.TS.TablaSimbolo import TablaSimbolo
+from Interprete.Instrucciones.Break import Break
+from Interprete.TS.Tipo import Tipo
+
+class If(Instruccion):
+
+    def __init__(self, condicion, ins_if, ins_elseif, ins_else, fila, columna):
+        self.condicion = condicion
+        self.ins_if = ins_if
+        self.ins_elseif = ins_elseif
+        self.ins_else = ins_else
+        self.fila = fila
+        self.columna = columna
+        self.flag = False;
+
+    def interpretar(self, tree, table):
+        cond = self.condicion.interpretar(tree, table)
+        if isinstance(cond, Exception): return cond
+
+        if self.condicion.tipo == Tipo.BOOLEANO:
+            
+            if bool(cond) == True:
+                new_table = TablaSimbolo(table)
+                for instruccion in self.ins_if:
+                    result = instruccion.interpretar(tree, new_table)
+                    if isinstance(result, Exception):
+                        tree.get_excepcion().append(result)
+                        tree.update_consola(result.__str__())
+                    if isinstance(result, Break): return result
+            else:
+                if self.ins_elseif != None:
+                    new_table = TablaSimbolo(table)
+                    for instruccion in self.ins_elseif:
+                        result = instruccion.interpretar(tree, new_table)
+                        if(self.flag == False):
+                            self.flag = instruccion.condicion.interpretar(tree, new_table)
+                        if isinstance(result, Exception):
+                            tree.get_excepcion().append(result)
+                            tree.update_consola(result.__str__()) 
+                        # if isinstance(result, Break): return result
+                        if(self.flag == True): break
+
+                if self.ins_else != None and self.flag != True:
+                    new_table = TablaSimbolo(table)
+                    for instruccion in self.ins_else:
+                        result = instruccion.interpretar(tree, new_table)
+                        if isinstance(result, Exception):
+                            tree.get_excepcion().append(result)
+                            tree.update_consola(result.__str__())       
+                        # if isinstance(result, Break): return result
+
+        else:
+            return Exception("Semantico", "Tipo de Dato no Booleano en If.", self.fila, self.columna)
+
+    def AST(self):
+        pass
+
